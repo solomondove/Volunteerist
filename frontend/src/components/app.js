662b9bad93bf68533a4aee4b6e7203800f7dfa59
k3vross <k3vross@gmail.com>
2020-08-31 12:02:33 -0700

    add logout and more fields to auth

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -5,15 +5,21 @@ import { Switch } from "react-router-dom";
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import MainPage from './main/main_page';
+import ProfileContainer from './profile/profile_container';
+import NavBarContainer from './nav/nav_bar_container';
 
 
 const App = () => (
     <div>
+        <NavBarContainer />
         <Switch>
             <AuthRoute exact path="/" component={MainPage} />
             <AuthRoute exact path="/login" component={LoginFormContainer} />
             <AuthRoute exact path="/signup" component={SignupFormContainer} />
+
+            <ProtectedRoute exact path="/profile" component={ProfileContainer} />
         </Switch>
+
     </div>
 );
 
