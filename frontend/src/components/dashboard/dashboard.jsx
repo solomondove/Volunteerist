fcb20e2275a4dac8e6fb2fe98ad067fffe71e026
Camille Fogg <camille.m.fogg@gmail.com>
2020-09-01 21:22:58 -0400

    Add message schema

--- frontend/src/components/dashboard/dashboard.jsx
+++ frontend/src/components/dashboard/dashboard.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class Dashboard extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-
 
     render() {
         return (
