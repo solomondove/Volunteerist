b361748ef5611afd80cbbda58dbb32eb11fca14d
birdmaaan <eb.d@outlook.com>
2020-09-01 22:53:54 -0700

    change production file path

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const db = require("./config/keys").mongoURI;
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
   })
 }
 
