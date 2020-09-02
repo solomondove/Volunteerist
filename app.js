de13582273f63df7aab0a7899fa5e7898bd9fd1e
birdmaaan <eb.d@outlook.com>
2020-09-02 11:13:47 -0700

    change app.js url

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ const app = express();
 const db = require("./config/keys").mongoURI;
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/public'));
+  app.use(express.static('frontend/build'));
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   })
 }
 
