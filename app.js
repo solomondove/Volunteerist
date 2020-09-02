3b08e2bfcbf6f514f5cc58a6bfff9903603e8785
birdmaaan <eb.d@outlook.com>
2020-09-02 09:28:08 -0700

    edit json

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 const db = require("./config/keys").mongoURI;
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
+  app.use(express.static('frontend/public'));
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
   })
