5e70669635bd28a3df65c151a6c39b5bedf1632f
Camille Fogg <camille.m.fogg@gmail.com>
2020-09-03 02:41:29 -0400

    Add splash and navbar styling and make ask show page

--- frontend/src/components/nav/nav_bar.js
+++ frontend/src/components/nav/nav_bar.js
@@ -18,12 +18,27 @@ class NavBar extends React.Component {
         if (this.props.loggedIn) {
             return (
                 <div className="navbar-right">
-                    <Link to={"/profile"}>Profile</Link>
-                    <Link to={"/asks/new"}>Create Ask</Link>
-                    <Link to={"/offers/new"}>Create Offer</Link>
-                    <Link to={"/asks"}>All Asks</Link>
-                    <Link to={"/offers"}>All Offers</Link>
-                    <button onClick={this.logoutUser}>Logout</button>
+                    <div className="navbar-trigger-volunteer">
+                        <div className="navbar-label-volunteer">Volunteer</div>
+                        <ul className="navbar-dropdown-volunteer">
+                            <Link to={"/offers/new"}>Create Offer</Link>
+                            <Link to={"/asks"}>All Asks</Link>
+                        </ul>
+                    </div>
+                    <div className="navbar-trigger-ask">
+                        <div className="navbar-label-ask">Ask</div>
+                        <ul className="navbar-dropdown-volunteer">
+                            <Link to={"/asks/new"}>Create Ask</Link>
+                            <Link to={"/offers"}>All Offers</Link>
+                        </ul>
+                    </div>
+                    <div className="navbar-trigger-dashboard">
+                        <div className="navbar-label-dashboard">Dashboard</div>
+                        <ul className="navbar-dropdown-dashboard">
+                            <Link to={"/profile"}>Profile</Link>
+                            <button onClick={this.logoutUser}>Logout</button>
+                        </ul>
+                    </div>
                 </div>
             );
         } else {
