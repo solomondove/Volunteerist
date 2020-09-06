62ff61101f4e3378c167507f750b257e067ec6c9
k3vross <k3vross@gmail.com>
2020-09-06 15:46:14 -0700

    add footer

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch } from "react-router-dom";
+import {FaGithub} from 'react-icons/fa';
 
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
@@ -19,27 +20,50 @@ import EditOfferContainer from "./asks_offers/edit_offer_container";
 import CompleteAskFormContainer from "./asks_offers/complete_ask_form_container";
 
 const App = () => (
-    <div className='app'>
-        <NavBarContainer /> 
-        <Switch>
-            
-            <AuthRoute exact path="/" component={MainPage} />
-            <AuthRoute exact path="/login" component={LoginFormContainer} />
-            <AuthRoute exact path="/signup" component={SignupFormContainer} />
+  <div className="app">
+    <NavBarContainer />
+    <Switch>
+      <AuthRoute exact path="/" component={MainPage} />
+      <AuthRoute exact path="/login" component={LoginFormContainer} />
+      <AuthRoute exact path="/signup" component={SignupFormContainer} />
 
-            <ProtectedRoute exact path="/profile" component={ProfileContainer} />
-            <ProtectedRoute exact path="/dashboard" component={DashboardContainer} />
-            <ProtectedRoute exact path="/asks" component={AsksIndexContainer} />
-            <ProtectedRoute exact path="/asks/new" component={AskFormContainer} />
-            <ProtectedRoute exact path="/offers" component={OffersIndexContainer} />
-            <ProtectedRoute exact path="/offers/new" component={OfferFormContainer} />
-            <ProtectedRoute exact path="/asks/edit/:ask_id" component={EditAskContainer} />
-            <ProtectedRoute exact path="/offers/edit/:offer_id" component={EditOfferContainer} />
-            <ProtectedRoute exact path="/asks/completed/:ask_id" component={CompleteAskFormContainer} />
-            <ProtectedRoute exact path="/asks/:ask_id" component={AskShowContainer} />
-            <ProtectedRoute exact path="/offers/:offer_id" component={OfferShowContainer} />
-        </Switch>
-    </div>
+      <ProtectedRoute exact path="/profile" component={ProfileContainer} />
+      <ProtectedRoute exact path="/dashboard" component={DashboardContainer} />
+      <ProtectedRoute exact path="/asks" component={AsksIndexContainer} />
+      <ProtectedRoute exact path="/asks/new" component={AskFormContainer} />
+      <ProtectedRoute exact path="/offers" component={OffersIndexContainer} />
+      <ProtectedRoute exact path="/offers/new" component={OfferFormContainer} />
+      <ProtectedRoute
+        exact
+        path="/asks/edit/:ask_id"
+        component={EditAskContainer}
+      />
+      <ProtectedRoute
+        exact
+        path="/offers/edit/:offer_id"
+        component={EditOfferContainer}
+      />
+      <ProtectedRoute
+        exact
+        path="/asks/completed/:ask_id"
+        component={CompleteAskFormContainer}
+      />
+      <ProtectedRoute exact path="/asks/:ask_id" component={AskShowContainer} />
+      <ProtectedRoute
+        exact
+        path="/offers/:offer_id"
+        component={OfferShowContainer}
+      />
+    </Switch>
+    <footer>
+      <div className="contact-footer">
+        <a href="https://github.com/solomondove" target="_blank"> <FaGithub /> Solomon Dove</a>
+        <a href="https://github.com/birbmaaan" target="_blank"> <FaGithub /> Elijah Dove</a>
+        <a href="https://github.com/camillemonet" target="_blank"> <FaGithub /> Camille Fogg</a>
+        <a href="https://github.com/k3vross" target="_blank"> <FaGithub /> Kevin Ross</a>
+      </div>
+    </footer>
+  </div>
 );
 
 export default App;
